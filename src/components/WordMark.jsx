9afcd26720eea2dca8d250e9bbcf36074d684199
Riccardo Pisano <riccardopisano02@gmail.com>
2026-07-23 17:12:33 +0200

    changed the favicon website to the right one

--- src/components/WordMark.jsx
+++ src/components/WordMark.jsx
@@ -1,7 +1,7 @@
 export default function Wordmark() {
     return (
         <h1 className="hero-wordmark">
-            {"OTTONII".split("").map((l, i) => (
+            {"OTTONI".split("").map((l, i) => (
                 <span key={i} className="letter" style={{ animationDelay: `${0.05 + i * 0.1}s` }}>{l}</span>
             ))}
         </h1>
